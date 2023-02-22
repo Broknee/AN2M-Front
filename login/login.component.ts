04bc63da5e6bc55b3b5b6883ba6fa9333095bbc7
Broknee <70056881+Broknee@users.noreply.github.com>
2023-02-22 22:59:14 +0100

    Création page login

---
+++ login/login.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  
+  
+  // contactForm : FormGroup;
+
+
+  // constructor(private fb: FormBuilder) { 
+
+  //   this.contactForm = this.fb.group({
+  //     mail: [''],
+  //     mdp: [''],
+  
+  //   });
+  // }
+
+  // onSubmit() {
+
+  // }
+  
+
+}
